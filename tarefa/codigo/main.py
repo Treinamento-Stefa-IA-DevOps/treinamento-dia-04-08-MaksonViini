549bf7b03baeea477f35ed6c97300102ab4c08c0
MaksonViini <maksonvinicio7@gmail.com>
2021-08-04 23:08:29 -0300

    Criando funcao para predizer a classe

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,32 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
+
+
 @app.post('/model')
-## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+# Coloque seu codigo na função abaixo
+async def titanic(Sex: int, Age: float, Lifeboat: int, Pclass: int):
+    with open('model/Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
 
+    pred = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()[0]
+
+    try:
+        return {
+            'survived': pred,
+            'status': 200,
+            'message': 'Valor predito com sucesso!',
+        }
+    except Exception:
+        return {
+            'message': 'Internal server error'
+        }
+
+
 @app.get('/model')
-def get():
-    return {'hello':'test'}
+async def get():
+    return {
+        'hello': 'test'
+    }
