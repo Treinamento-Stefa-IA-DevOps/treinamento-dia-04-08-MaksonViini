0027a537a0cae0648354020560538bca4cad567b
MaksonViini <maksonvinicio7@gmail.com>
2021-08-06 20:38:37 -0300

    Ajustando try except

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -8,19 +8,26 @@ app = FastAPI()
 @app.post('/model')
 # Coloque seu codigo na função abaixo
 async def titanic(Sex: int, Age: float, Lifeboat: int, Pclass: int):
-    with open('model/Titanic.pkl', 'rb') as fid:
-        titanic = pickle.load(fid)
-
-    pred = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()[0]
-
+    """
+    Sex: Sexo da pessoa. EX: 0/1, Parametro binario
+    Age: Idade da pessoa. Ex: 35
+    Lifeboat: Numero do barco salva vidas. EX: 1, 2, 3...
+    Pclass: Classe no navio. EX: 1 - Primeira classe, 2 - Segunda classe ...
+    """
     try:
+        with open('model/Titanic.pkl', 'rb') as fid:
+            titanic = pickle.load(fid)
+
+        pred = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()[0]
+
         return {
             'survived': pred,
             'status': 200,
             'message': 'Valor predito com sucesso!',
         }
-    except Exception:
+    except Exception as exception:
         return {
+            'Error': exception,
             'message': 'Internal server error'
         }
 
